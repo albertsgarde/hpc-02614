d561be51e59b52060fcad9e59891be9d77b6e043
OskarHD <oskar.h.dinesen@gmail.com>
2023-01-10 16:30:39 +0100

    Updated gauss_seidel.c with an inner calculation of the norm, to avoid storing a full copy of U.

--- assignment2/Poisson3D/gauss_seidel.c
+++ assignment2/Poisson3D/gauss_seidel.c
@@ -11,28 +11,35 @@ gauss_seidel(double ***U, double ***f, int N, int kmax, double threshold) {
 
     while (d > threshold && k < kmax) {
 
-        U = U;
-        gauss_seidel_inner(U, U, f, N);
-        d = frobeniusnorm(U, U, (N+2));
+        double old_u_val = 0;
+        double old_sum = 0;
+
+        double one_sixth = 1/6;
+        double gridsizeSq = 1/(N*N) ;
+    
+        for (int i = 1; i < (N + 2); i++){
+            for (int j=1; j < (N + 2); j++){
+                for (int k=1; k < (N + 2); k++){
+
+                    old_u_val = U[i][j][k];
+
+                    newval = (U[i-1][j][k] + U[i+1][j][k] + U[i][j-1][k] + U[i][j]+1[k] + U[i][j][k-1] + U[i][j][k+1])
+    
+                    U[i][j][k] = one_sixth * (newval + gridsizeSq * f[i][j][k]);
+
+                    old_sum += old_u_val * old_u_val;
+                }
+            }
+        }
+
+
+        d = Math.sqrt(old_sum);
         k += 1;
     }
 
-}
-
-void gauss_seidel_inner(double ***U, double ***U, double ***f, int N) {
-
-    double one_sixth = 1/6;
-    double gridsizeSq = 1/(N*N) ;
     
-    for (int i = 1; i < (N + 2); i++){
-        for (int j=1; j < (N + 2); j++){
-            for (int k=1; k < (N + 2); k++){
 
-                newval = (U[i-1][j][k] + U[i+1][j][k] + U[i][j-1][k] + U[i][j]+1[k] + U[i][j][k-1] + U[i][j][k+1])
-  
-                U[i][j][k] = one_sixth * (newval + gridsizeSq * f[i][j][k]);
-            }
-        }
-    }
+
+
 
 }
