06581089d02470b1c17b4f73bd5f52dc7a48b07b
Albert Garde <albertsgarde@gmail.com>
2023-01-10 17:27:07 +0100

    :bug: Also set edges to NAN

--- assignment2/Poisson3D/init_u.c
+++ assignment2/Poisson3D/init_u.c
@@ -12,14 +12,24 @@ void init_u_internal(const int N, double ***const u, const double start_T) {
 }
 
 void init_u_corners(const int N, double ***const u) {
-    u[0][0][0] = NAN;
-    u[0][0][N+1] = NAN;
-    u[0][N+1][0] = NAN;
-    u[0][N+1][N+1] = NAN;
-    u[N+1][0][0] = NAN;
-    u[N+1][0][N+1] = NAN;
-    u[N+1][N+1][0] = NAN;
-    u[N+1][N+1][N+1] = NAN;
+    for (int i = 0; i < N+2; ++i) {
+        u[i][0][0] = NAN;
+        u[i][0][N+1] = NAN;
+        u[i][N+1][0] = NAN;
+        u[i][N+1][N+1] = NAN;
+    }
+    for (int j = 0; j < N+2; ++j) {
+        u[0][j][0] = NAN;
+        u[0][j][N+1] = NAN;
+        u[N+1][j][0] = NAN;
+        u[N+1][j][N+1] = NAN;
+    }
+    for (int k = 0; k < N+2; ++k) {
+        u[0][0][k] = NAN;
+        u[0][N+1][k] = NAN;
+        u[N+1][0][k] = NAN;
+        u[N+1][N+1][k] = NAN;
+    }
 }
 
 void init_u_boundary_ass(const int N, double ***const u) {
