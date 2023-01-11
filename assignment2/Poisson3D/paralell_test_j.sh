5a5db72e848e69847868faa7004e9748c392ceb4
Camila Celes <camilaceles@gmail.com>
2023-01-11 13:48:18 +0100

    fix to bash script

--- assignment2/Poisson3D/paralell_test_j.sh
+++ assignment2/Poisson3D/paralell_test_j.sh
@@ -2,14 +2,14 @@
 #BSUB -J jacobi_par
 #BSUB -o jacobi_par_%J.out
 #BSUB -q hpcintro
-#BSUB -n 32
+#BSUB -n 16
 #BSUB -R "rusage[mem=2048]"
-#BSUB -W 1:00
+#BSUB -W 0:10
 
 rm parallel_j_results.dat
 
-NS="1 2 4 8 16 32"
-for n in NS
+NS="1 2 4 8 16"
+for n in $NS
 do
-  OMP_NUM_THREADS=$n ./poisson_j 100 10000 0 0.0 > parallel_j_results.dat
+  OMP_NUM_THREADS=$n ./poisson_j 100 10000 0 0.0 >> parallel_j_results.dat
 done
