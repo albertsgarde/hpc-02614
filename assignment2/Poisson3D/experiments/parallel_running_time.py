44f181c340897ef132e16c0cdb89e1f4102ffbbb
Albert Garde <albertsgarde@gmail.com>
2023-01-12 17:08:15 +0100

    :sparkles: Add experiment support for parallel-v3 algorithms and NUMA flags

--- assignment2/Poisson3D/experiments/parallel_running_time.py
+++ assignment2/Poisson3D/experiments/parallel_running_time.py
@@ -1,7 +1,9 @@
-from experiment import RunConfig, experiment
+from experiment import RunConfig, experiment, configs_to_df
 
 num_threads_list = [1] + list(range(2, 17, 2))
-configs = [RunConfig(gauss_seidel=gauss_seidel, n=100, iter_max=10000, tolerance=0, parallel=parallel, num_threads=num_threads) for num_threads in num_threads_list for parallel in range(0, 3) for gauss_seidel in [True, False]]
+parallel_list = [0, 1, 2, 3]
+gauss_seidel_list = [True, False]
+configs = [RunConfig(gauss_seidel=gauss_seidel, n=200, iter_max=2000, tolerance=0, parallel=parallel, num_threads=num_threads, proc_bind=proc_bind) for proc_bind in ["close", "spread"] for num_threads in num_threads_list for parallel in range(0, 3) for gauss_seidel in [True, False]]
 
 
 experiment(configs, "data.csv")
--- assignment2/Poisson3D/submit_batch.sh
+++ assignment2/Poisson3D/submit_batch.sh
@@ -3,14 +3,17 @@
 #BSUB -o poisson_%J.out
 #BSUB -e poisson_%J.err
 #BSUB -q hpcintro
-#BSUB -n 1
-#BSUB -R "rusage[mem=128]"
+#BSUB -n 24
+#BSUB -R "rusage[mem=512]"
 #BSUB -W 1:00
-#BSUB -R "span[hosts=1] affinity[socket(1)]"
+#BSUB -R "span[hosts=1]"
+
 
 module load gcc/12.2.0-binutils-2.39
 make realclean
 make
+echo "Activating python environment."
 . ../../env/bin/activate
+echo "Running experiment script."
 python3 experiments/parallel_running_time.py
 
