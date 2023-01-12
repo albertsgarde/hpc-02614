7ef47165a009f320702ae19a66d3761d53aff539
Camila Celes <camilaceles@gmail.com>
2023-01-12 13:13:51 +0100

    add plots

--- assignment2/Poisson3D/plot.py
+++ assignment2/Poisson3D/plot.py
@@ -9,6 +9,7 @@ from matplotlib.colors import SymLogNorm
 import pandas as pd
 import seaborn as sns
 from pathlib import Path
+import numpy as np
 
 if len(sys.argv) < 3:
     raise Exception("Script takes two arguments: plot.py <data_path> <out_path>")
@@ -17,12 +18,37 @@ data_path = sys.argv[1]
 out_path = sys.argv[2]
 
 df = pd.read_csv(data_path)
+is_gauss_seidel = df['gauss_seidel']
+df.loc[df['gauss_seidel'] == True, 'gauss_seidel'] = "gauss_seidel"
+df.loc[df['gauss_seidel'] == False, 'gauss_seidel'] = "jacobi"
 
+base_time_gs = df.loc[(df['gauss_seidel'] == "gauss_seidel") & (df['num_threads'] == 1), "time"].values[0]
+base_time_j = df.loc[(df['gauss_seidel'] == "jacobi") & (df['num_threads'] == 1), "time"].values[0]
 
-plot_path = os.path.join(out_path, "out.pdf")
+df.loc[df['gauss_seidel'] == "gauss_seidel", "speedup"] = base_time_gs/df.loc[:,"time"]
+df.loc[df['gauss_seidel'] == "jacobi", "speedup"] = base_time_j/df.loc[:,"time"]
+
+plot_path = os.path.join(out_path, "out.png")
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plot = sns.lineplot(df, x="n", y="num_iterations", hue="gauss_seidel", style="gauss_seidel", markers=True, dashes=False, ax=ax)
+
+f = 0.98
+num_threads = df.num_threads.unique()
+amdahl = 1.0/((1.0 - f) + f/num_threads)
+sns.lineplot(x=num_threads, y=amdahl, color="orange", label=f"amdahl f={f}")
+f = 0.78
+num_threads = df.num_threads.unique()
+amdahl = 1.0/((1.0 - f) + f/num_threads)
+sns.lineplot(x=num_threads, y=amdahl, color="blue", label=f"amdahl f={f}")
+
+
+sns.lineplot(data=df, x="num_threads", y="num_threads", color="green", label="linear",linestyle='--', ax=ax)
+plot = sns.lineplot(data=df, x="num_threads", y="speedup", hue="gauss_seidel", style="gauss_seidel", linestyle='', marker='o', dashes=False, ax=ax)
+
 ax.yaxis.set_major_formatter(ScalarFormatter())
+ax.legend(title="")
+ax.set_xlabel("Num. threads")
+ax.set_ylabel("Speedup")
+ax.set_title("Speedup for N=100")
 plot.get_figure().savefig(plot_path)
